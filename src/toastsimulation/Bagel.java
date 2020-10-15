8b9cd309ad29a1b6f06b97036e611cab29343608
EmilianoHuerta <maxhuerta014@gmail.com>
2020-10-15 15:51:27 -0500

    creating bagel object -- still bread

---
+++ src/toastsimulation/Bagel.java
@@ -0,0 +1,47 @@
+package toastsimulation;
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Ellipse;
+import edu.macalester.graphics.GraphicsGroup;
+import java.awt.Color;
+
+public class Bagel {
+    private double centerX;
+    private double centerY;
+    private double maxX;
+    private double maxY;
+
+    private Ellipse bagelBread;
+    private final double BALL_RADIUS = 50.5; 
+    public static final Color breadColor = new Color(255,255,140);
+
+    public Bagel(
+        double centerX,
+        double centerY,
+        double maxX,
+        double maxY
+        ){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.maxX = maxX;
+        this.maxY = maxY; 
+        Ellipse bagelBread = new Ellipse(centerX - BALL_RADIUS, centerY - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2);
+        this.bagelBread = bagelBread;
+        bagelBread.setStrokeColor(breadColor);
+        bagelBread.setFillColor(breadColor);
+
+    }
+    public void addToCanvas(CanvasWindow canvas) {
+        canvas.add(bagelBread);
+
+    }
+
+    /**
+     * Removes the bagel shape from the given canvas.
+     *  @param canvas represents canvas window 
+     */
+    public void removeFromCanvas(CanvasWindow canvas) {
+        canvas.remove(bagelBread);
+    }
+    
+}
