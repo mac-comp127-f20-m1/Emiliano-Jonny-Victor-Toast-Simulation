6b7cb91ea935dd764161c63a3ab46400d94ea7d5
EmilianoHuerta <maxhuerta014@gmail.com>
2020-10-15 15:51:16 -0500

    creating time boxes -- might not be needed

--- src/toastsimulation/TimeBoxes.java
+++ src/toastsimulation/TimeBoxes.java
@@ -4,15 +4,21 @@ import edu.macalester.graphics.Rectangle;
 import java.awt.Color;
 
 public class TimeBoxes extends Rectangle{
-    public TimeBoxes(double xPosition, double yPosition, double width, double height){
+    private TimeBoxes timebox;
+
+    public TimeBoxes(TimeBoxes timebox, double xPosition, double yPosition, double width, double height){
         super(xPosition, yPosition, width, height);
         setStrokeWidth(Math.rint((width + height) / 40 + 1) * 0.5);
         setActive(false);
+        this.timebox = timebox;
     }
 
      public void setActive(boolean active) {
         setFillColor(active
             ? new Color(0x3ba634)
             : new Color(0xD9D9D9));
+    }
+    public TimeBoxes getForecast() {
+        return timebox;
     }
 }
